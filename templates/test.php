f10b3737ef85795acc53c4a9d6058c5d594ffb8e
Bini <steinkamp@netcologne.de>
2012-07-11 16:31:57 +0200

    first commit netbeans

---
+++ templates/test.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
+<head>
+<title>Mein Templatesystem:: {$title}</title>
+<meta http-equiv="Content-Type" content="text/xhtml; charset=ISO-8859-1" />
+</head>
+<body>
+<h1>{L_START}</h1>
+Hallo {$name}. Der Aktuelle Timestamp lautet: {$time}
+{* Ein Template einbinden *}
+{include file="othertemplate.tpl"}
+</body>
+</html>
